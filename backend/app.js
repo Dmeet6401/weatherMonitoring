9bdd794b56841110562e618ffd5e0c94e6a4f3b3
Dmeet6401 <desai.meet06@gmail.com>
2024-10-21 01:51:51 +0530

    port

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const { initSocket } = require('./services/socket'); // Import the socket logic
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.use(cors());
 
 // Middleware
