0266baac6ee98218751ed75867402a60d3d4e33c
Dmeet6401 <desai.meet06@gmail.com>
2024-10-21 02:00:09 +0530

    fix

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,10 @@ const { initSocket } = require('./services/socket'); // Import the socket logic
 
 const app = express();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 app.use(cors());
 
 // Middleware
@@ -28,13 +31,17 @@ initCronJobs(); // Call the function to initialize both cron jobs
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB connected');
+    // More logs to debug the server starting process
+    console.log('Starting server...');
     
-    // Start the Express server
-    const server = http.createServer(app); // Create a server instance
-    initSocket(server); // Initialize Socket.IO
+    const server = http.createServer(app); 
+    initSocket(server);
 
     server.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });    
   })
-  .catch(error => console.error('MongoDB connection error:', error));
+  .catch(error => {
+    console.error('MongoDB connection error:', error);
+    process.exit(1); // Exit the process if the DB connection fails
+  });
